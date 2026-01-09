1fb915857d7a5311b50edbfb20c4cf8ac3db1cc7
Papp Mico <miklos.papp@guidance.hu>
2026-01-09 16:21:27 +0100

    Add "Muveletek.ipynb" notebook for task breakdowns, refactor Mars Lander logic, and update Docker volume mapping.

--- mars-lander-2/megoldas.py
+++ mars-lander-2/megoldas.py
@@ -2,19 +2,19 @@ import sys
 import math
 
 
-def tavolsag_tup(hely1, hely2):
+def tavolsag_pont(hely1, hely2):
     # Euklideszi tavolsag ket pont (x, y) kozott
     return math.sqrt((hely1[0] - hely2[0]) ** 2 + (hely1[1] - hely2[1]) ** 2)
 
 
-def tavolsag_pont(c1, c2):
+def tavolsag(c1, c2):
     # Kulonbseg ket skalar kozott (itt x-koordinata tavolsaga)
     return c1 - c2
 
 
-def irany_sebesseg(h_seb):
+def irany_sebesseg(vizszintes_sebesseg):
     # Vizszintes sebesseg elojele alapjan visszaadja az iranyt
-    if h_seb < 0:
+    if vizszintes_sebesseg < 0:
         return -1
     else:
         return 1
@@ -26,6 +26,63 @@ def cel_irany(x, cel_x):
         return 1
     else:
         return -1
+
+
+def mozgasi_fazis(x, vizszintes_sebesseg, fordulopont, leszallas_x):
+    toloero = 4
+    if x < fordulopont:
+        forgatas = -30
+    else:
+        forgatas = 30
+    if leszallas_x[0] < x < leszallas_x[1] and abs(vizszintes_sebesseg) < 2:
+        forgatas = 0
+        return forgatas, toloero, False, True, fordulopont
+    return forgatas, toloero, True, False, fordulopont
+
+
+def leszallasi_fazis(fuggoleges_sebesseg):
+    if abs(fuggoleges_sebesseg) < 36:
+        toloero = 2
+    else:
+        toloero = 4
+    forgatas = 0
+    return forgatas, toloero
+
+
+def ellensulyozas(kezdeti_stabilizalas_szukseges, kezdeti_vizszintes_sebesseg, vizszintes_sebesseg, fuggoleges_sebesseg, cel_gyors_h, leszallas_x, x, cel, mozgas, leszallas, fordulopont, pi):
+    if kezdeti_stabilizalas_szukseges:
+        toloero = 4
+        irany = irany_sebesseg(kezdeti_vizszintes_sebesseg)
+        try:
+            alap_szog = int(math.asin(cel_gyors_h / toloero) * 180 / pi)
+        except Exception:
+            alap_szog = 0
+        forgatas = irany * (alap_szog + 2)
+        if fuggoleges_sebesseg > 0:
+            toloero = 2
+        if abs(vizszintes_sebesseg) < 2:
+            forgatas = 0
+            kezdeti_stabilizalas_szukseges = False
+            if leszallas_x[0] < x < leszallas_x[1]:
+                leszallas = True
+            else:
+                mozgas = True
+                fordulopont = (x + cel[0]) // 2
+            if mozgas:
+                f, t, mozgas, leszallas, fordulopont = mozgasi_fazis(x, vizszintes_sebesseg, fordulopont, leszallas_x)
+                return f, t, kezdeti_stabilizalas_szukseges, mozgas, leszallas, fordulopont
+            if leszallas:
+                f, t = leszallasi_fazis(fuggoleges_sebesseg)
+                return f, t, kezdeti_stabilizalas_szukseges, mozgas, leszallas, fordulopont
+        return forgatas, toloero, kezdeti_stabilizalas_szukseges, mozgas, leszallas, fordulopont
+    else:
+        if mozgas:
+            f, t, mozgas, leszallas, fordulopont = mozgasi_fazis(x, vizszintes_sebesseg, fordulopont, leszallas_x)
+            return f, t, kezdeti_stabilizalas_szukseges, mozgas, leszallas, fordulopont
+        if leszallas:
+            f, t = leszallasi_fazis(fuggoleges_sebesseg)
+            return f, t, kezdeti_stabilizalas_szukseges, mozgas, leszallas, fordulopont
+        return 0, 4, kezdeti_stabilizalas_szukseges, mozgas, leszallas, fordulopont
 
 
 elozo_x, elozo_y, felszin_x, felszin_y = 0, 0, 1, 1
@@ -46,19 +103,20 @@ print('Cel kijelolve - {}'.format(cel), file=sys.stderr)
 gravitacio = 3.711  # Mars gravitacio
 pi = math.pi
 kezdo_tav_x = -1
-kezdo_h_seb = -1
-kezdo_v_seb = -1
+kezdeti_vizszintes_sebesseg = -1
+kezdeti_fuggoleges_sebesseg = -1
 kezdes_szamitas = True  # Kezdeti szamitas: cel gyorsulas/idok es kiindulo ertekek
-kezdo_mozgas_ellent = False  # Jelezi, hogy ellensulyozni kell-e a kezdeti vizszintes sebesseget
+ellensulyozni_kell = False  # Jelezi, hogy ellensulyozni kell-e a kezdeti vizszintes sebesseget
 leszallas = False  # Leszallasi fazis
 mozgas = False  # Mozgasi fazis
+fordulopont = 0
 
 # Jatek ciklus
 while True:
-    # Bemenet: x y h_seb v_seb uzemanyag forgatasi_szog teljesitmeny
-    x, y, h_seb, v_seb, uzemanyag, forgatasi_szog, teljesitmeny = [int(i) for i in input().split()]
+    # Bemenet: x y vizszintes_sebesseg fuggoleges_sebesseg uzemanyag forgatasi_szog teljesitmeny
+    x, y, vizszintes_sebesseg, fuggoleges_sebesseg, uzemanyag, forgatasi_szog, teljesitmeny = [int(i) for i in input().split()]
 
-    tav_x = tavolsag_pont(x, cel[0])
+    tav_x = tavolsag(x, cel[0])
 
     # Vizszintes es fuggoleges gyorsulasi komponensek (teljesitmeny es forgatasi szog alapjan)
     gyors_h = teljesitmeny * math.sin(forgatasi_szog * (pi / 180))
@@ -67,62 +125,40 @@ while True:
     # Kezdo szamitasok
     if kezdes_szamitas:
         # Kiindulo ertekek
-        kezdo_tav_x = abs(tavolsag_pont(x, cel[0]))
-        kezdo_h_seb = h_seb
-        kezdo_v_seb = v_seb
+        kezdo_tav_x = abs(tavolsag(x, cel[0]))
+        kezdeti_vizszintes_sebesseg = vizszintes_sebesseg
+        kezdeti_fuggoleges_sebesseg = fuggoleges_sebesseg
         # Cel ertekek
-        cel_gyors_h = (kezdo_h_seb ** 2) / (2 * kezdo_tav_x) if kezdo_tav_x != 0 else 0  # Vizszintes gyorsulas
+        cel_gyors_h = (kezdeti_vizszintes_sebesseg ** 2) / (2 * kezdo_tav_x) if kezdo_tav_x != 0 else 0  # Vizszintes gyorsulas
         try:
-            cel_ido = (2 * kezdo_tav_x) // (kezdo_h_seb)
+            cel_ido = (2 * kezdo_tav_x) // (kezdeti_vizszintes_sebesseg)
         except Exception:
             pass
-        if abs(cel_gyors_h) < 2 and kezdo_h_seb != 0:
+        if abs(cel_gyors_h) < 2 and kezdeti_vizszintes_sebesseg != 0:
             print(0, 4)
             continue
         kezdes_szamitas = False  # Kesz a kezdeti kalkulacio
-        if kezdo_h_seb != 0:
-            kezdo_mozgas_ellent = True
+        if kezdeti_vizszintes_sebesseg != 0:
+            ellensulyozni_kell = True
         else:
             mozgas = True
             fordulopont = (x + cel[0]) // 2
 
-    # Stabilizalas kezdeti mozgassal szemben
-    if kezdo_mozgas_ellent is True:
-        toloero = 4  # Fix toloero a gyors fekezeshez
-        irany = irany_sebesseg(kezdo_h_seb)  # Ellensulyozzuk az indulasi iranyt
-        forgatas = irany * (int(math.asin(cel_gyors_h / toloero) * 180 / pi) + 2)
-        if v_seb > 0:
-            toloero = 2
-        if abs(h_seb) < 2:
-            forgatas = 0
-            kezdo_mozgas_ellent = False  # Stabilizaltuk a vizszintes mozgast
-            # Eldontjuk, hogy zonaban vagyunk-e
-            if leszallas_x[0] < x < leszallas_x[1]:  # Leszallasi zonaban vagyunk
-                leszallas = True
-            else:  # Tulcsuszas vagy tul korai fekezes
-                mozgas = True
-                fordulopont = (x + cel[0]) // 2
-
-    # Mozgasi fazis
-    if mozgas is True:
-        toloero = 4
-        if x < fordulopont:
-            forgatas = -30
-        elif x >= fordulopont:
-            forgatas = 30
-
-        if leszallas_x[0] < x < leszallas_x[1] and abs(h_seb) < 2:
-            mozgas = False
-            leszallas = True
-            mozgas_irany = -1
-            forgatas = 0
-
-    # Leszallasi eljaras
-    if leszallas is True:
-        if abs(v_seb) < 36:
-            toloero = 2
-        else:
-            toloero = 4
+    # Fazisvezerles: ellensulyozas + mozgas/leszallas delegalva egy helyre
+    forgatas, toloero, ellensulyozni_kell, mozgas, leszallas, fordulopont = ellensulyozas(
+        ellensulyozni_kell,
+        kezdeti_vizszintes_sebesseg,
+        vizszintes_sebesseg,
+        fuggoleges_sebesseg,
+        cel_gyors_h,
+        leszallas_x,
+        x,
+        cel,
+        mozgas,
+        leszallas,
+        fordulopont,
+        pi
+    )
 
     # Diagnosztikai uzenetek a hibakereseshez
     print('Aktualis cel: {}'.format(cel), file=sys.stderr)
